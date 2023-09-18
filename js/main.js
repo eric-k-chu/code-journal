9c854706eee19676562b7f61483b9994fff6ce67
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-18 20:11:39 +0000

    prevents the page from refreshing when the form is submitted

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const $photoUrl = document.querySelector('#photo-url');
 const $photo = document.querySelector('.photo-wrapper > img');
+const $form = document.querySelector('form');
 
 $photoUrl.addEventListener('input', function (event) {
   $photo.src = $photoUrl.value;
+});
+
+$form.addEventListener('submit', function (event) {
   event.preventDefault();
 });
