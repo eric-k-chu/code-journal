5a44d4fe17cf493937d6cea35cea9eeba1bbb563
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-18 20:32:58 +0000

    stores the data model as a JSON in local storage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,8 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', function (event) {
+  const dataJSON = JSON.stringify(data);
+  this.localStorage.getItem('javascript-local-storage', dataJSON);
+});
