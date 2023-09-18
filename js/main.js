f0c65d2857ea12fff8a2a19b496a20dc31e028bd
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-18 20:24:41 +0000

    created a new entry object with all form input values and a unique entry id. Pushed new entry into data entries array and resetted the form on submit

--- js/main.js
+++ js/main.js
@@ -8,4 +8,14 @@ $photoUrl.addEventListener('input', function (event) {
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
+  const entry = {
+    title: $form[0].value,
+    photoUrl: $form[1].value,
+    notes: $form[2].value,
+    entryID: data.nextEntryId,
+  };
+  data.nextEntryId++;
+  data.entries.push(entry);
+  $photo.src = 'images/placeholder-image-square.jpg';
+  $form.reset();
 });
